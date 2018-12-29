9d6629539d81ce676566061577d91c4e170f01c2
Vivek Roy <thezealousfool@gmail.com>
2018-12-29 21:46:39 +0530

    Changed div to h1 (to trigger netlify build)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import {html, render} from 'lit-html';
 
 // This is a lit-html template function. It returns a lit-html template.
-const helloTemplate = (name) => html`<div>Hello ${name}!</div>`;
+const helloTemplate = (name) => html`<h1>Hello ${name}!</h1>`;
 
 // This renders <div>Hello Steve!</div> to the document body
 render(helloTemplate('Vivek'), document.body);
