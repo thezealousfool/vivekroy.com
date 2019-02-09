f119932e8e7a19518dddd12a5dbaf11c58411c81
Vivek Roy <thezealousfool@gmail.com>
2019-02-10 03:34:25 +0530

    Simple routing with xhr

--- src/js/components.js
+++ src/js/components.js
@@ -24,10 +24,7 @@ wickedElements.define('.route-dispatcher', {
             event.preventDefault();
             var newEvent = new CustomEvent('handleRoute', {
                 bubbles: false,
-                detail: {
-                    href: event.detail.href,
-                    get: event.detail.get,
-                }
+                detail: event.detail,
             });
             elem.dispatchEvent(newEvent);
         }
@@ -35,7 +32,49 @@ wickedElements.define('.route-dispatcher', {
 });
 
 wickedElements.define('.route-handler', {
+    onconnected: function(event) {
+        window.onpopstate = (function (event) {
+            this.onhandleRoute({
+                detail: {
+                    href: event.target.location.pathname,
+                    to: event.state.to,
+                    get: event.state.get,
+                },
+                push: false,
+            }, false);
+        }).bind(this);
+    },
     onhandleRoute: function (event) {
-        xhr()
-    }
+        if (event.push)
+            this.loadView(event.detail, event.push);
+        else
+            this.loadView(event.detail, true);
+    },
+    loadView: function(detail, push) {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = (function(event) {
+            var newDoc = event.target.response;
+            var newNode = newDoc.querySelector(detail.get);
+            var oldNode = document.querySelector(detail.get);
+            if (!oldNode || !newNode) return;
+            oldNode.parentNode.replaceChild(newNode, oldNode);
+            if (push) {
+                history.replaceState({
+                    to: detail.to,
+                    get: detail.get,
+                }, null, window.location.pathname);
+                history.pushState({
+                    to: detail.to,
+                    get: detail.get,
+                }, null, detail.href);
+            }
+            this.el.dispatchEvent(new CustomEvent('routeChanged', {
+                bubbles: false,
+                detail,
+            }));
+        }).bind(this);
+        xhr.responseType = 'document';
+        xhr.open('GET', detail.href);
+        xhr.send();
+    },
 });
