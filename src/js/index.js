bd465a82e8722b7e33664516a8710d225c59fa0e
Vivek Roy <thezealousfool@gmail.com>
2019-02-12 08:22:44 +0530

    Fixed: Caching not working due to presence of .html

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function (event) {
     document.querySelector('.route-dispatcher').dispatchEvent(new CustomEvent('cacheRoute', {
         bubbles: false,
         detail: {
-            href: ['/about.html', '/contact.html', '/'],
+            href: ['/about', '/contact', '/'],
         },
     }));
 });
