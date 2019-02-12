d7dbe76f7b19ebda27ec605ee57351dbab5cdbe4
Vivek Roy <thezealousfool@gmail.com>
2019-02-12 09:51:20 +0530

    Page based caching

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,15 @@
 window.addEventListener('load', function (event) {
+    var caches = null;
+    switch (window.location.pathname) {
+        case '/':
+        case '/about':
+        case '/contact': caches = ['/', '/about', '/contact']; break;
+        default: caches = [];
+    };
     document.querySelector('.route-dispatcher').dispatchEvent(new CustomEvent('cacheRoute', {
         bubbles: false,
         detail: {
-            href: ['/about', '/contact', '/'],
+            href: caches,
         },
     }));
 });
